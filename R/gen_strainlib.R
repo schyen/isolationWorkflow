6ad7efb7308f72c8f1a645042cfc33ab90050f51
Sandi Yen <sandi.yen@gmail.com>
2018-05-31 12:03:19 -0400

    fixed error in identifying species that need alignment check

--- R/gen_strainlib.R
+++ R/gen_strainlib.R
@@ -77,7 +77,8 @@ gen_strainlib <- function(blast_file, lib_path, lib_file = NULL,
   wip_df <- blast[!blast$match %in% add_singles,]
 
   # getting entries from strain library
-  wip_df <- rbind(wip_df, lib_df[!lib_df$match %in% add_singles,])
+  wip_df <- rbind(wip_df, lib_df[lib_df$match %in%
+                                   species_tally$match[species_tally$Freq > 1],])
 
   msg <- sprintf("\nGoing through current blast file and strain library, multiple strains of %s were found. Generating fasta file for this species for sequence alignment", unique(wip_df$match))
 
